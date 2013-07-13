07a2cb052c7daa822d44126c321968426b65fd83
Jónatan Nilsson <jonatan@nilsson.is>
2013-07-13 08:23:07 +0000

    Slightly fixed indentation.

--- src/db/engine.py
+++ src/db/engine.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 import settings
 
 engine = create_engine('mysql://%s:%s@%s/%s' % (settings.MYSQL_USER,
-												settings.MYSQL_PASSWORD,
-												settings.MYSQL_HOST,
-												settings.MYSQL_DATABASE))
+                                                settings.MYSQL_PASSWORD,
+                                                settings.MYSQL_HOST,
+                                                settings.MYSQL_DATABASE))
 Base = declarative_base()
 
 #Base.metadata.create_all(engine) 

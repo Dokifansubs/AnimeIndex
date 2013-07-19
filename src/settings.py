a966f286bdbd16237b76e2ad7a8f986ea18973ba
Jónatan Nilsson <jonatan@nilsson.is>
2013-07-19 12:35:45 +0000

    Added more settings to settings.py.

--- src/settings.py
+++ src/settings.py
@@ -3,4 +3,19 @@ MYSQL_USER = "user"
 MYSQL_PASSWORD = "mypassword"
 MYSQL_DATABASE = "database"
 
-TITLE = "Site Title"
+TITLE = "Anime Index"
+SECRET_KEY = "my ultra secret string."
+SESSION_KEY = "this is absolutely gonna be my ultra secret type of key!"
+COOKE_KEY = "This is my cookie key to sign stuff."
+
+CAPTCHA_PUBLIC_KEY = "6Le8qeQSAAAAAARmO_t7zOarKIbvSFUEeJ7h_kre"
+CAPTCHA_PRIVATE_KEY = "6Le8qeQSAAAAAGeJ5KlRxKbNxGKtj-Wfmd179GBU"
+
+ADMIN = "admin@localhost"
+
+SMTP_HOST = "smtp-host"
+SMTP_PORT = 25
+#SMTP_USE_TLS = True
+#SMTP_USE_AUTHENTICATION = True
+#SMTP_USER = "username"
+#SMTP_PASSWORD = "password"

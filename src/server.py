17879c407a6123a381b419c0e6c996cb89709611
Jónatan Nilsson <jonatan@nilsson.is>
2013-07-19 12:33:01 +0000

    Added app and session support as well as 404 handler. (500 handler commented out for debugging)

--- src/server.py
+++ src/server.py
@@ -1,9 +1,13 @@
-from bottle import route, run, template, Bottle, static_file, install
+import bottle
+from bottle import route, run, template, Bottle, static_file, install, request, error, debug
 from bottle.ext import sqlalchemy
 from lesscss import LessCSS
+from beaker.middleware import SessionMiddleware
 
 from db import *
 from common import render, default
+from views import *
+import settings
 
 LessCSS(media_dir='static', exclude_dirs=['img', 'src'], based=False, compressed=False)
 
@@ -17,5 +21,42 @@ def index(db):
     news = db.query(News).order_by(News.id.desc())[0:3]
     return render(news=news)
 
+@error(404)
+def error404(error):
+    return render("message", message=[
+        "The requested page or file was not found.",
+        """If you're trying to open a torrent file, you either don't have permission
+           to view it or the torrent in question got deleted."""])
+
+
+"""
+@error(500)
+def error500(error):
+    send_email("error",
+               "%s - %s" % (settings.TITLE, error.status),
+               settings.ADMIN,
+               path=request.path,
+               exception=error.exception,
+               traceback=error.traceback)
+
+    return render("message", message=[
+        "An unknown error occured while processing your request.",
+        "This error has been logged and our code monkeys will do \
+         all in their power to fix this."])
+"""
+
+
 install(sqlalchemy.SQLAlchemyPlugin(engine, Base.metadata, create=True))
-run(host='localhost', port=8080)
+
+session_opts = {
+    'session.type': 'file',
+    'session.cookie_expires': 300,
+    'session.data_dir': './data',
+    'session.auto': True,
+    'session.secret': settings.SESSION_KEY
+}
+
+app = bottle.default_app()
+app = SessionMiddleware(app, session_opts)
+
+run(app=app, host='localhost', reloader=True, port=8080)
